cb54661d7d340cd3a0fd462588dbd7663d0bd7df
Ihor Pavliuk <iktus2230@gmail.com>
2022-06-17 01:52:36 +0300

    Created calendar

---
+++ src/components/calendar/build.js
@@ -0,0 +1,19 @@
+import { DateTime } from "luxon"
+
+export default function buildCalendar(selectedDay) {
+  const calendar = []
+  const monthStartDay = selectedDay.startOf('month').minus({ days: 1 }).startOf('week')
+  const monthEndDay = selectedDay.endOf('month').endOf('week')
+
+  let calendarDay = monthStartDay.startOf('day')
+  while (calendarDay <= monthEndDay.startOf('day')) {
+    let week = Array(7).fill(0);
+    for (let i = 0; i < week.length; i++) {
+      week[i] = calendarDay
+      calendarDay = calendarDay.plus({ days: 1 })
+    }
+    calendar.push(week)
+  }
+
+  return calendar;
+}
